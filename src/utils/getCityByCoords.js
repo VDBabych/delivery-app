3fe5527db437ec756c765b7375ec817cf791f03f
VDBabych <113364212+VDBabych@users.noreply.github.com>
2023-11-28 21:52:06 +0200

    Update getCityByCoords.js

--- src/utils/getCityByCoords.js
+++ src/utils/getCityByCoords.js
@@ -9,7 +9,7 @@ const getCityByCoords = async (coords) => {
   const { data } = await axiosInstance.get("geocode/json?", {
     params: {
       latlng: `${coords.latitude},${coords.longitude}`,
-      key: import.meta.env.VITE_PUBLIC_KEY,
+      key: process.env.VITE_PUBLIC_KEY,
       language: "uk",
     },
   });
